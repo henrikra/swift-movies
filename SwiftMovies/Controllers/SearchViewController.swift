00392d7b0723321da065740f5d178b5ed3f93e7b
Henrik Raitasola <henrik.raitasola@gmail.com>
2018-02-08 22:14:12 +0200

    Add text field and listen to its values

--- SwiftMovies/Controllers/SearchViewController.swift
+++ SwiftMovies/Controllers/SearchViewController.swift
@@ -9,9 +9,53 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+  let searchInputContainerView: UIView = {
+    let view = UIView()
+    view.translatesAutoresizingMaskIntoConstraints = false
+    return view
+  }()
+  
+  let searchTextField: UITextField = {
+    let textField = UITextField()
+    textField.placeholder = "Search for movies"
+    textField.translatesAutoresizingMaskIntoConstraints = false
+    textField.backgroundColor = .white
+    textField.layer.cornerRadius = 5
+    textField.leftViewMode = .always
+    textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: Spacing.padding400, height: 0))
+    textField.addTarget(self, action: #selector(searchForMovies(_:)), for: .editingChanged)
+    return textField
+  }()
+  
+  let closeButton: UIButton = {
+    let button = UIButton(type: .system)
+    button.addTarget(self, action: #selector(closeSearch), for: .touchUpInside)
+    button.translatesAutoresizingMaskIntoConstraints = false
+    button.setTitle("Close", for: .normal)
+    button.setTitleColor(.white, for: .normal)
+    return button
+  }()
+  
+  @objc func closeSearch() {
+    searchTextField.endEditing(true)
+    dismiss(animated: true, completion: nil)
+  }
+  
+  @objc func searchForMovies(_ textField: UITextField) {
+    print(textField.text)
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    
     view.addGradientBackground(fromColor: Colors.primary500, toColor: Colors.secondary500)
+    view.addSubview(searchInputContainerView)
+    searchInputContainerView.addSubview(searchTextField)
+    searchInputContainerView.addSubview(closeButton)
+    
+    view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "|[v0]|", options: [], metrics: metrics, views: ["v0": searchInputContainerView]))
+    view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-40-[v0(50)]", options: [], metrics: metrics, views: ["v0": searchInputContainerView]))
+    view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "|-padding500-[v0]-padding500-[v1]-padding500-|", options: [], metrics: metrics, views: ["v0": searchTextField, "v1": closeButton]))
+    view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: [], metrics: metrics, views: ["v0": searchTextField]))
+    view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: [], metrics: metrics, views: ["v0": closeButton]))
   }
 }
