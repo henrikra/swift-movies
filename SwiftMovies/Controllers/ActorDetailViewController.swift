965547a4937f11c6f1b986c649243b98517fafa5
Henrik Raitasola <henrik.raitasola@gmail.com>
2018-02-24 18:28:36 +0200

    Remove last back button title

--- SwiftMovies/Controllers/ActorDetailViewController.swift
+++ SwiftMovies/Controllers/ActorDetailViewController.swift
@@ -101,6 +101,7 @@ class ActorDetailViewController: UIViewController, UINavigationControllerDelegat
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    navigationItem.title = ""
     _ = view.addGradientBackground(fromColor: Colors.primary500, toColor: Colors.secondary500, startPoint: CGPoint(x: 1, y: 0.5))
     navigationController?.delegate = self
     tableView.register(SearchResultCell.self, forCellReuseIdentifier: cellId)
